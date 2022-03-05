6b646ef7a628713975a0b92b96ecd64fe10a6111
yaokaisei <kaisei.design@gmail.com>
2022-03-05 19:51:25 +0900

    ESlintのエラーを一時的に無視

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,13 @@ import type { Answers } from './types/answers';
 
 function App() {
   const [answers, setAnswers] = useState<Answers[]>([]); // 回答コンポーネントに表示するデータ
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [chats, setChats] = useState([]); // チャットコンポーネントに表示するデータ
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [currentId, setCurrentId] = useState('init'); // 現在の質問ID
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [dataset, setDataset] = useState<DataSet>(defaultDataset); // 質問と回答のデータセット
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [open, setOpen] = useState(false); // 問い合わせフォーム用モーダルの開閉を管理
 
   useEffect(() => {
